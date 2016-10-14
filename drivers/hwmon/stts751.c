b7892ca0a87f092736da9fc6cbcf6009c9c11b6e
Andrea Merello <andrea.merello@gmail.com>
2016-10-14 15:36:11 +0200

    hwmon: stts751: make driver less chatty
    Suggested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Andrea Merello <andrea.merello@gmail.com>

--- drivers/hwmon/stts751.c
+++ drivers/hwmon/stts751.c
@@ -577,7 +577,7 @@ static int stts751_detect(struct i2c_client *new_client,
 	default:
 		return -ENODEV;
 	}
-	dev_info(&new_client->dev, "Chip %s detected!", name);
+	dev_dbg(&new_client->dev, "Chip %s detected!", name);
 
 	rev_id = i2c_smbus_read_byte_data(new_client, STTS751_REG_REV_ID);
 
