2a66d6708edcc147dea37a3fe9eac15c6d388b26
Andrea Merello <andrea.merello@gmail.com>
2017-01-10 11:00:31 +0100

    hwmon: stts751: remove extra newline
    Suggested-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Andrea Merello <andrea.merello@gmail.com>

--- drivers/hwmon/stts751.c
+++ drivers/hwmon/stts751.c
@@ -402,7 +402,6 @@ static ssize_t show_therm(struct device *dev, struct device_attribute *attr,
 	return snprintf(buf, PAGE_SIZE - 1, "%d\n", priv->therm);
 }
 
-
 static ssize_t set_therm(struct device *dev, struct device_attribute *attr,
 		       const char *buf, size_t count)
 {
@@ -431,7 +430,6 @@ static ssize_t show_hyst(struct device *dev, struct device_attribute *attr,
 
 	return snprintf(buf, PAGE_SIZE - 1, "%d\n", priv->therm - priv->hyst);
 }
-
 
 static ssize_t set_hyst(struct device *dev, struct device_attribute *attr,
 		       const char *buf, size_t count)
